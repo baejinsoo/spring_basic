cac74e7d7cd6e6e612e56eba636f721be7a1cbf3
baejinsoo <jinsoo941010@naver.com>
2021-03-01 03:21:19 +0900

    주문, 할인 도메인 개발 및 테스트

---
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,24 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void CreateOrder() {
+        Long memberId = 2L; //long memberId = 2L => 프리미티브 타입으로 선언하면 null을 못 넣음
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
