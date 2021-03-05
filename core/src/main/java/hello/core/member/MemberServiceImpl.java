74774fc9885867081272173cdad05330b75af298
baejinsoo <jinsoo941010@naver.com>
2021-03-06 01:32:36 +0900

    DIP 해결 - appconfig 생성

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,12 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository; // 추상화에만 의존하게됨, 구현 객체는 모르고 있음
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,15 +1,26 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+// import hello.core.discount.FixDiscountPolicy;
+// import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //  private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); 기존 할인 정책
+//  private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); 변경된 할인 정책, 이렇게 하면 DIP 위반
+//  현재는 추상 클래스(DiscountPolicy)와 구현 클래스(FixDiscountPolicy()) 모두에 의존해 있다. 이를 추상 클래스에만 의존하도록 바꿔야함
+//  해결방안 : 인터페이스에만 의존하도록 설계와 코드 변경 후, 클라이언트인(OrderServiceImpl)에 DiscountPolicy의 구현 객체를 대신 생성하고 주입해줘야함
+    private final DiscountPolicy discountPolicy; // DIP는 지켰지만, 구체 객체가 없기때문에 null 에러 발생
+    private final MemberRepository memberRepository; // => AppConfig의 생성자 주입을 통해 해결
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
